afd64c2bae18b2d532308fe3a0a784d2bdaf7e7e
imStoorm <imstoormdev@proton.me>
2024-08-06 22:47:25 -0300

    chore: added shadcn/ui and set the theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-red-500">Hello World!</h1>
+    <Button>Teste</Button>
   );
 }
